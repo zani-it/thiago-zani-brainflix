29b0fd2e172d9e4a0273c877dc9e5de7cfd433ed
Zani <zanit@me.com>
2023-03-05 04:01:05 -0500

    feature | comments
    comment section styles mobile

--- src/components/VideoComments.jsx
+++ src/components/VideoComments.jsx
@@ -3,43 +3,63 @@ import avatarImage from "..//assets/images/Mohan-muruge.jpg";
 
 export default function VideoComments({ comments }) {
   return (
-    <div className="video__comments">
-      <article>input area</article>
-      <h3>3 Comments</h3>
-      <div>
-        <img className="nav__avatar" src={avatarImage} alt="Avatar"/>
-      </div>
-      <div>
-        <label for="comment-input" class="comment__label">
-          JOIN THE CONVERSATION
-        </label>
-        <textarea
-          class="comment__text-area"
-          type="text"
-          id="comment-input"
-          name="comment box"
-          placeholder="Add a new comment">
-        </textarea>
-        <span id="form-error"></span>
-        <button className="comment__button"> COMMENT</button>
-      </div>
-      {comments.map((comments) => (
-        <div key={comments.id} className="comment">
-          <div>
-            <h3>{comments.name}</h3>
-            <h3>
-              {new Date(comments.timestamp).toLocaleDateString("en-US", {
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric",
-              })}
-            </h3>
-          </div>
-          <div>
-            <p>{comments.comment}</p>
+    <div className="comments">
+      <article className="comments__section">
+        <div className="comments__input">
+          <h4>3 Comments</h4>
+          <div className="comments__input--box">
+            <div>
+              <img
+                className="comments__avatar"
+                src={avatarImage}
+                alt="Avatar"
+              />
+            </div>
+            <div>
+              <label for="comment-input" class="comments__label">
+                JOIN THE CONVERSATION
+              </label>
+              <textarea
+                class="comments__input-el"
+                type="text"
+                id="comment-input"
+                name="comment box"
+                placeholder="Add a new comment"
+              ></textarea>
+              <span id="form-error"></span>
+              <button className="comments__button"> COMMENT</button>
+            </div>
           </div>
         </div>
-      ))}
+
+        {comments.map((comments) => (
+          <div key={comments.id} className="comment">
+            <div className="comments__item">
+              <img
+                className="comments__avatar"
+                src={avatarImage}
+                alt="Avatar"
+              />
+              <div>
+                <div className="comments__header">
+                  <h4>{comments.name}</h4>
+                  <div className="comments__date">
+                    {new Date(comments.timestamp).toLocaleDateString("en-US", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    })}
+                  </div>
+                </div>
+
+                <div>
+                  <p>{comments.comment}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </article>
     </div>
   );
 }
