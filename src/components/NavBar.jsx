d008ea4bb7977f40f8dd44e5574132b0c8e53ad1
Zani <zanit@me.com>
2023-03-05 01:43:13 -0500

    feature | navBar
    mobile styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import navLogo from "../assets/images/Logo/BrainFlix-logo.svg";
 import avatarImage from "..//assets/images/Mohan-muruge.jpg";
+import searchIcon from '../assets/images/Icons/search.svg';
 
 export default function NavBar() {
 
 
   return (
-    <nav>
-        <div>
+    <nav className="nav">
+        <div class="nav__content">
             <img className="nav__logo" src={navLogo} alt="logo" />
-            <div className="nad__input--container">
-            <input type="text" />
+            <div className="nav__input--container">
+            <input className="nav__input-el" type="text" placeholder="Search" id="shearch video" />
             <img className="nav__avatar" src={avatarImage} alt="Avatar" />
             </div>
-            <button className="nav__upload-button"> UPLOAD </button>
+            <button className="nav__button"> UPLOAD </button>
         </div>
-
     </nav>
   );
 }
